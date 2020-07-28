40d4c5992f564024b16cc1663fce4c1f765eabe3
Brandon Davis <bdavis171@gmail.com>
2020-07-28 10:57:17 -0500

    created react app

---
+++ pokemon-battle-2-app/src/components/AppContainer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1>Pokemon Battle #2</h1>
+            </div>
+         );
+    }
+}
+ 
+export default AppContainer;
